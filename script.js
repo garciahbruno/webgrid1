70a56ad31750587b341587b6e97a4adc9c99bc6b
Bruno <lovehenny23@gmail.com>
2025-05-21 15:21:02 -0400

    stuff changed

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const grid = document.querySelector('.grid');
 
+const grid = document.querySelector('.grid');
 const emojis = ['💳', '📱', '🧾', '🎲', '💵', '🎨', '🖼️', '🧠', '💡', '📷', '🌐'];
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 2500; i++) {
   const item = document.createElement('div');
   item.className = 'item';
   item.textContent = emojis[i % emojis.length];
   grid.appendChild(item);
 }
+window.onload = () => {
+  // Scroll to the center of the page
+  window.scrollTo(
+    document.body.scrollWidth / 2 - window.innerWidth / 2,
+    document.body.scrollHeight / 2 - window.innerHeight / 2
+  );
+};
